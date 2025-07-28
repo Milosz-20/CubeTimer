b2ffc5aee3103da86bf8a2863bec2025cb1a1f11
Milosz Karpeta <miloszkarpeta@gmail.com>
2025-07-28 11:27:58 +0200

    feat: add axios dependency feat: implement active state styling in Sidebar component feat: create basic files for future development of user authentication

--- apps/web/src/components/layout/Sidebar/Sidebar.tsx
+++ apps/web/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,34 +1,46 @@
 import React from "react";
 import styles from "@components/layout/Sidebar/Sidebar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Clock, BarChart2, Bell, Settings } from "lucide-react";
 
 export const Sidebar: React.FC = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <aside className={styles.sidebar}>
       <div className={styles.title}>
         <h2>Cube Timer</h2>
       </div>
       <ul>
-        <li className={styles.listItem}>
+        <li
+          className={`${styles.listItem} ${isActive("/timer") ? styles.active : ""}`}
+        >
           <Link to="/timer">
             <Clock size={20} />
             Timer
           </Link>
         </li>
-        <li className={styles.listItem}>
+        <li
+          className={`${styles.listItem} ${isActive("/statistics") ? styles.active : ""}`}
+        >
           <Link to="/statistics">
             <BarChart2 size={20} />
             Statistics
           </Link>
         </li>
-        <li className={styles.listItem}>
+        <li
+          className={`${styles.listItem} ${isActive("/notifications") ? styles.active : ""}`}
+        >
           <Link to="/notifications">
             <Bell size={20} />
             Notifications
           </Link>
         </li>
-        <li className={styles.listItem}>
+        <li
+          className={`${styles.listItem} ${isActive("/settings") ? styles.active : ""}`}
+        >
           <Link to="/settings">
             <Settings size={20} />
             Settings
